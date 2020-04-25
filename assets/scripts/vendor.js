f273827d380f3d811af25eb55bff9b451eabf2e0
Lucica Arba <arba.lucica@gmail.com>
2020-04-25 20:36:48 +0200

    add catch errors

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,11 +1,11 @@
-const monsterHealthBar = document.getElementById("monster-health");
-const bonusLifeEl = document.getElementById("bonus-life");
-const playerHealthBar = document.getElementById("player-health");
+const monsterHealthBar = document.getElementById('monster-health');
+const playerHealthBar = document.getElementById('player-health');
+const bonusLifeEl = document.getElementById('bonus-life');
 
-const attackBtn = document.getElementById("attack-btn");
-const strongAttackBtn = document.getElementById("strong-attack-btn");
-const healBtn = document.getElementById("heal-btn");
-const logBtn = document.getElementById("log-btn");
+const attackBtn = document.getElementById('attack-btn');
+const strongAttackBtn = document.getElementById('strong-attack-btn');
+const healBtn = document.getElementById('heal-btn');
+const logBtn = document.getElementById('log-btn');
 
 function adjustHealthBars(maxLife) {
   monsterHealthBar.max = maxLife;
@@ -16,24 +16,23 @@ function adjustHealthBars(maxLife) {
 
 function dealMonsterDamage(damage) {
   const dealtDamage = Math.random() * damage;
-  monsterHealthBar.value -= dealtDamage;
+  monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
   return dealtDamage;
 }
 
 function dealPlayerDamage(damage) {
   const dealtDamage = Math.random() * damage;
-  playerHealthBar.value -= dealtDamage;
+  playerHealthBar.value = +playerHealthBar.value - dealtDamage;
   return dealtDamage;
 }
 
 function increasePlayerHealth(healValue) {
-  playerHealthBar.value += healValue;
+  playerHealthBar.value = +playerHealthBar.value + healValue;
 }
 
 function resetGame(value) {
   playerHealthBar.value = value;
   monsterHealthBar.value = value;
-  alert('Reset, Next round!')
 }
 
 function removeBonusLife() {
